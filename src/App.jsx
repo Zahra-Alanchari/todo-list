dcd6404c174d71001c441c24a83241968ad1c369
Zahra Alanchari <zahraalanchari@Zahras-MacBook-Pro.local>
2024-06-24 13:58:08 +0330

    implementing header

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+import desktopImage from "../images/bg-desktop-dark.jpg";
+import Header from "./components/header";
+import NewTodo from "./components/new-todo";
+{
+  /* <img src={desktopImage} alt="background-image" /> */
 }
-
-export default App
+export default function App() {
+  return (
+    <div className="main">
+      <div className="main-box">
+        <Header />
+        <NewTodo />
+      </div>
+    </div>
+  );
+}
---
+++ src/components/new-todo.jsx
@@ -0,0 +1,11 @@
+export default function NewTodo() {
+  return (
+    <div>
+      <input
+        className="creat-todo"
+        type="text"
+        placeholder="Create a new todo..."
+      />
+    </div>
+  );
+}
