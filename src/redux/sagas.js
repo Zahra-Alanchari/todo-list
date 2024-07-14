a9cce1d5553e8dc74a9d3f75583fc9fe1375bd82
Zahra Alanchari <zahraalanchari@Zahras-MacBook-Pro.local>
2024-07-14 17:56:17 +0330

    clean codes

--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -36,7 +36,6 @@ function* fetchTodoSaga() {
   }
 }
 function* chechTodoSaga(action) {
-  // console.log(action, "cccc");
   try {
     const res = yield call(
       fetch,
@@ -48,10 +47,9 @@ function* chechTodoSaga(action) {
       }
     );
     const data = yield res.json();
-    // console.log(data,"dataaa")
     yield put({ type: CHECK_TODO_SUCCESS, payload: data });
   } catch (err) {
-    console.log("ey baba");
+    console.log(err);
   }
 }
 function* deleteTodoSaga() {
